3e497a26cb70295150be1f8cfa050a91a14a4cb9
animullai <andrewsimonmull@gmail.com>
2025-10-24 06:07:15 -0400

    Update _cldy.js

--- netlify/functions/_cldy.js
+++ netlify/functions/_cldy.js
@@ -1,6 +1,8 @@
 // netlify/functions/_cldy.js
 // Shared helpers for restore.js / upscale.js
 
+const crypto = require('crypto');
+
 // Extract Cloudinary public_id from a full URL
 // e.g. https://res.cloudinary.com/<cloud>/image/upload/v123/folder/photo.jpg -> "folder/photo"
 exports.parsePublicId = function parsePublicId(cloudinaryUrl) {
@@ -13,8 +15,14 @@ exports.parsePublicId = function parsePublicId(cloudinaryUrl) {
   return dot !== -1 ? tail.slice(0, dot) : tail;       // folder/photo
 };
 
-// Build Basic auth header for Cloudinary API
-exports.buildBasicAuthHeader = function buildBasicAuthHeader(key, secret) {
-  const token = Buffer.from(`${key}:${secret}`).toString('base64');
-  return `Basic ${token}`;
+// Build Upload API signature: SHA1 of sorted params + API_SECRET
+exports.buildSignature = function buildSignature(params, apiSecret) {
+  // Build string of non-empty params sorted by key, like: eager=...&public_id=...&timestamp=...&type=upload
+  const kv = Object.entries(params)
+    .filter(([, v]) => v !== undefined && v !== null && v !== '')
+    .sort(([a], [b]) => a.localeCompare(b))
+    .map(([k, v]) => `${k}=${v}`)
+    .join('&');
+
+  return crypto.createHash('sha1').update(kv + apiSecret).digest('hex');
 };
